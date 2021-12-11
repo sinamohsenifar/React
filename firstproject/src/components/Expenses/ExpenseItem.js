d295a6ff80d1b88cc78dbc495b868dc44dc99cc5
sina <sinamohsenifar@gmail.com>
2021-12-11 20:19:19 +0330

    first project: change item with useState function

--- firstproject/src/components/Expenses/ExpenseItem.js
+++ firstproject/src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ExpenseItem.css';
 import Card from '../UI/Card'
 import ExpenseDate from './ExpenseDate'
@@ -10,15 +10,25 @@ const ExpenseItem = (props) => {
     // const expenseDate = new Date(2012, 2, 28);
     // const expenseTitle = 'Car Insurance';
     // const expenseAmount = 294.67;
+    
+    // const [date, setDate] = useState(props.date)
+    // const [amount, setAmount] = useState(props.amount)
+    const [title, setTitle] = useState(props.title);
+    let date = props.date
+    let amount = props.amount
 
+    const ChangeClickHandler = () => {
+        setTitle('Changed title');
+    }
 
     return (
         <Card className='expense-item'>
-            <ExpenseDate date={props.date} />
+            <ExpenseDate date={date} />
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
-                <div className='expense-item__price'>${props.amount}</div>
+                <h2>{title}</h2>
+                <div className='expense-item__price'>${amount}</div>
             </div>
+            <button onClick={ChangeClickHandler}>Change Title</button>
         </Card>
     )
 }
